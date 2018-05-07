0653a8ce3e55f84b1394e174b291dee120d9f830
choi9798 <wai9798tw@gmail.com>
2018-05-07 22:08:43 +0800

    ip input argument

--- hw2/multicast_client_server/multicast_server.c
+++ hw2/multicast_client_server/multicast_server.c
@@ -5,15 +5,20 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 struct in_addr localInterface;
 struct sockaddr_in groupSock;
 int sd;
+char ip[50];
 char databuf[1024] = "Multicast test message lol!";
 int datalen = sizeof(databuf);
+FILE *fp;
+
  
 int main (int argc, char *argv[ ])
 {
 /* Create a datagram socket on which to send. */
+	strcpy(ip, argv[1]);
 	sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sd < 0)
 	{
@@ -30,24 +35,11 @@ int main (int argc, char *argv[ ])
 	groupSock.sin_addr.s_addr = inet_addr("226.1.1.1");
 	groupSock.sin_port = htons(5555);
 	 
-	/* Disable loopback so you do not receive your own datagrams.
-	{
-	char loopch = 0;
-	if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch)) < 0)
-	{
-	perror("Setting IP_MULTICAST_LOOP error");
-	close(sd);
-	exit(1);
-	}
-	else
-	printf("Disabling the loopback...OK.\n");
-	}
-	*/
 	 
 	/* Set local interface for outbound multicast datagrams. */
 	/* The IP address specified must be associated with a local, */
 	/* multicast capable interface. */
-	localInterface.s_addr = inet_addr("192.168.0.108");
+	localInterface.s_addr = inet_addr(ip);
 	if(setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0)
 	{
 	  perror("Setting local interface error");
@@ -55,6 +47,14 @@ int main (int argc, char *argv[ ])
 	}
 	else
 	  printf("Setting the local interface...OK\n");
+
+	fp = fopen("1.jpg","rb");
+	if(fp==NULL)
+	{
+		printf("File open error");
+	}
+
+
 	/* Send a message to the multicast group specified by the*/
 	/* groupSock sockaddr structure. */
 	/*int datalen = 1024;*/
@@ -65,18 +65,5 @@ int main (int argc, char *argv[ ])
 	else
 	  printf("Sending datagram message...OK\n");
 	 
-	/* Try the re-read from the socket if the loopback is not disable
-	if(read(sd, databuf, datalen) < 0)
-	{
-	perror("Reading datagram message error\n");
-	close(sd);
-	exit(1);
-	}
-	else
-	{
-	printf("Reading datagram message from client...OK\n");
-	printf("The message is: %s\n", databuf);
-	}
-	*/
 	return 0;
 }
